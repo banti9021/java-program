0f5b7eca555eb171f3e5b474adcd717b60601558
banti9021 <nbanti943@gmail.com>
2024-11-07 10:58:29 +0530

    up

--- class varible.java
+++ class varible.java
@@ -2,7 +2,9 @@ class Variable {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int sum = a + b;
+       int sum = a + b;
+
+
         System.out.println(sum);
     }
 }
